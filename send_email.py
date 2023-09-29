76365dad949c82e6785b1e50f5428fcd9995aa83
Sanjave P. Singh <60440140+Sanjave-P-Singh@users.noreply.github.com>
2023-09-29 17:11:44 -0400

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,17 +1,31 @@
-import smtplib, ssl
-import os
+import smtplib
 
 
 def send_email(message):
-    host = "smtp.gmail.com"
-    port = 465
+    # Your email sending logic here
+    try:
+        # Create an SMTP server instance
+        server = smtplib.SMTP('your_smtp_server_address', 587)  # Update with your SMTP server details
 
-    username = "sanjave.singh25@gmail.com"
-    password = os.getenv("PASSWORD")
+        # Start TLS encryption
+        server.starttls()
 
-    receiver = "sanjave.singh25@gmail.com"
-    context = ssl.create_default_context()
+        # Log in to the SMTP server with your credentials
+        server.login('your_email@example.com', 'your_password')  # Update with your email and password
 
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
+        # Send the email
+        sender = 'your_email@example.com'  # Update with your email address
+        receiver = 'recipient@example.com'  # Update with the recipient's email address
+        server.sendmail(sender, receiver, message)
+
+        # Close the SMTP server connection
+        server.quit()
+
+    except smtplib.SMTPResponseException as e:
+        print(f"SMTP Error: {e.smtp_code} - {e.smtp_error}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+# Call the send_email function with your message content
+send_email("Your email message content here")
