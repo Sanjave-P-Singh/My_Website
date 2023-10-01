35567ee52ee5adbea911cdc1889f84f11448177a
Sanjave P. Singh <60440140+Sanjave-P-Singh@users.noreply.github.com>
2023-09-30 21:18:28 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -17,11 +17,6 @@ with col2:
     """
     st.info(content)
 
-      content2 = """
-Below you can find some of the apps I have built in python. Feel free to contact me at:
-"""
-st.write(content2)
-
     email_address = "mailto:ssingh025@citymail.cuny.edu"
     linkedin_url = "https://www.linkedin.com/in/sanjave-singh-77043a162/"
     github_url = "https://github.com/Sanjave-P-Singh"
@@ -40,7 +35,11 @@ st.write(content2)
     st.markdown(
         f"<div style='text-align: center; margin-bottom: 24px'><a href='{github_url}' style='font-size: 24px;'>Github</a></div>",
         unsafe_allow_html=True)
-
+    
+content2 = """
+Below you can find some of the apps I have built in python. Feel free to contact me at the email mentioned above.
+"""
+st.write(content2)
 
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
