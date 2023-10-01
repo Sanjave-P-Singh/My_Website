2878baadd781d5ab32cb41e95ca03e111ff03301
Sanjave P. Singh <60440140+Sanjave-P-Singh@users.noreply.github.com>
2023-09-30 20:11:41 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -17,7 +17,26 @@ with col2:
     """
     st.info(content)
 
-content2 = """
+    email_address = "mailto:ssingh025@citymail.cuny.edu"
+    linkedin_url = "https://www.linkedin.com/in/sanjave-singh-77043a162/"
+    github_url = "https://github.com/Sanjave-P-Singh"
+
+    # Email Address link
+    st.markdown(
+        f"<div style='text-align: center; margin-bottom: 20px;'><a href='{email_address}' style='font-size: 22px;'>Email Address</a></div>",
+        unsafe_allow_html=True)
+
+    # LinkedIn link
+    st.markdown(
+        f"<div style='text-align: center; margin-bottom: 20px;'><a href='{linkedin_url}' style='font-size: 22px;'>LinkedIn</a></div>",
+        unsafe_allow_html=True)
+
+    # GitHub link
+    st.markdown(
+        f"<div style='text-align: center; margin-bottom: 20px'><a href='{github_url}' style='font-size: 22px;'>Github</a></div>",
+        unsafe_allow_html=True)
+
+    content2 = """
 Below you can find some of the apps I have built in python. Feel free to contact me!
 """
 st.write(content2)
@@ -27,14 +46,14 @@ col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 df = pandas.read_csv("data.csv",sep=";")
 
 with col3:
-    for index, row in df[:2].iterrows():
+    for index, row in df[:10].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
 
 with col4:
-    for index, row in df[2:4].iterrows():
+    for index, row in df[10:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
